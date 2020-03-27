12ee2d08861fec1e9bbcec7b9643e92a2ad86891
Alexandre da Costa Tavares Junior <alexandre.junior@paygo.corp>
2020-03-26 22:00:09 -0300

    Resolução Exercicio 2

---
+++ respExecicio02.py
@@ -0,0 +1,3 @@
+# 2. Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+n1 = str(input('Digite um número: '))
+print('O número informado foi {}'.format(n1))
