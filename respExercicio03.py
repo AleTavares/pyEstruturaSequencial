a060f930f0c89dcc361ca602d9d460f426217028
Alexandre da Costa Tavares Junior <alexandre.junior@paygo.corp>
2020-03-27 10:56:26 -0300

    Resolução Exercícios 3, 4 e 5

---
+++ respExercicio03.py
@@ -0,0 +1,6 @@
+# 3. Faça um Programa que peça dois números e imprima a soma.
+n1 = float(input('Digite o Primeiro Numero: '))
+n2 = float(input('Digite o Segundo Numero: '))
+soma = n1 + n2
+
+print('Resultado: ' + str(soma))
---
+++ respExercicio05.py
@@ -0,0 +1,4 @@
+# 5. Faça um Programa que converta metros para centímetros.
+metros = float(input('Digite a metragem: '))
+centimetros = str(metros * 100)
+print('Convertido pra Centimetros: ' + centimetros)
